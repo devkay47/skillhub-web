72172d74b47da3ae597421abe0dc933b2ed0baf7
Devkay_ <greenskies196@gmail.com>
2026-05-31 13:23:27 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import Browse from './pages/Browse.jsx'
+import SkillDetail from './pages/SkillDetail.jsx'
+import Submit from './pages/Submit.jsx'
+import Blog from './pages/Blog.jsx'
+import BlogPost from './pages/BlogPost.jsx'
+import NotFound from './pages/NotFound.jsx'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route path="/browse/:category" element={<Browse />} />
+        <Route path="/skills/:slug" element={<SkillDetail />} />
+        <Route path="/submit" element={<Submit />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
